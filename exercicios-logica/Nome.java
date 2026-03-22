4aafd22c040a60b280e3d9a4bd04c1184bf5aa33
Italo Silva <italosilvadsantos@gmail.com>
2026-03-22 20:35:28 -0300

    adicionando exercicio 01 - exercicios de logica

---
+++ exercicios-logica/Nome.java
@@ -0,0 +1,7 @@
+// Exercicio 01: Exibir uma mensagem na tela com o nome completo
+
+public class Nome {
+    public static void main(String[] args) {
+        System.out.println("Italo Silva dos Santos");
+    }
+}
