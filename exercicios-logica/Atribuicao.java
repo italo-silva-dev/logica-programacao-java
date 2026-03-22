ebbccad5bc3a38e61622b2715e2581b8a09fbdf3
Italo Silva <italosilvadsantos@gmail.com>
2026-03-22 20:37:51 -0300

    Adicionando Exercicio 03 - Exercícios de Lógica Java

---
+++ exercicios-logica/Atribuicao.java
@@ -0,0 +1,12 @@
+// Exercicio 03: Atribuir valores inteiros e exibir resultado de cálculo
+
+public class Atribuicao {
+    static void main (String [] args) {
+        int x, y, z;
+        x = 4;
+        y = 3;
+        z = 2;
+
+        System.out.println(x * y / z);
+    }
+}
