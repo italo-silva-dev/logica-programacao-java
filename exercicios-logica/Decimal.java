05b3a07614049098094ea747281a78b344faf448
Italo Silva <italosilvadsantos@gmail.com>
2026-03-22 20:36:40 -0300

    adicionando exercicio 02 - exercicios de logica

---
+++ exercicios-logica/Decimal.java
@@ -0,0 +1,12 @@
+// Exercicio 02: Definir variaveis com valores especificos e imprimi-las
+
+public class Decimal {
+    static void main (String [] args) {
+        double x, y;
+        x = 3.5;
+        y = 4.64;
+
+        System.out.println("Valor de X: " + x);
+        System.out.println("Valor de Y: " + y);
+    }
+}
