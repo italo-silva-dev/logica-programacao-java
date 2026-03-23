dcd10131bd44a317e350baa06d087a0ad2971dab
Italo Silva <italosilvadsantos@gmail.com>
2026-03-23 15:03:02 -0300

    adicionando ExemploCombinadoIF2.java

---
+++ conceitos-basicos/ExemploCombinadoIF2.java
@@ -0,0 +1,13 @@
+public class ExemploWhileCombinadoIF2 {
+    public static void main (String [] args) {;
+        while (true) {
+            System.out.println("Entre com um valor para x: ");
+            int x = new java.util.Scanner(System.in).nextInt();
+            if (x % 2 == 0) {
+                System.out.println("È par!");
+            } else {
+                System.out.println("É impar!");
+            }
+        }
+    } 
+}
