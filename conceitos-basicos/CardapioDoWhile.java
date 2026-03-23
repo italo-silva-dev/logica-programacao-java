b6e5d4cce470b2e7dc8bae8d6f692be66e1d2b77
Italo Silva <italosilvadsantos@gmail.com>
2026-03-23 16:22:36 -0300

    adicionando os algoritmos com os conceitos basicos

---
+++ conceitos-basicos/CardapioDoWhile.java
@@ -0,0 +1,35 @@
+public class CardapioDoWhile {
+    public static void main(String[] args) {
+        int opcao;
+        do {
+            System.out.println("Entre com a opção desejada! ");
+            System.out.println("1 - Pratos");
+            System.out.println("2 - Bebidas");
+            System.out.println("3 - Encerre sua conta");
+            opcao = new java.util.Scanner(System.in).nextInt();
+            switch (opcao) {
+                case 1: {
+                    System.out.println("* Hamburguer");
+                    System.out.println("* Pizza a moda da casa");
+                    System.out.println("* Batata recheada");
+                    break;
+                }
+                case 2: {
+                    System.out.println("* Agua");
+                    System.out.println("* Refrigernte");
+                    System.out.println("* Cerveja");
+                    break;
+                }
+
+                case 3: {
+                    System.out.println("Conta encerrada");
+                    break;
+                }
+
+                default: {
+                    System.out.println("Opcao invalida!");
+                }
+            }
+        } while (opcao != 3);
+    }
+}
