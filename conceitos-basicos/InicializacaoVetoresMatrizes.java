4d2f576e508e938629205a57f7a98b071dfd71ec
Italo Silva <italosilvadsantos@gmail.com>
2026-03-23 16:33:26 -0300

    adicionando os algoritmos com os conceitos basicos

---
+++ conceitos-basicos/InicializacaoVetoresMatrizes.java
@@ -0,0 +1,6 @@
+public class InicializacaoVetoresMatrizes {
+    static void main(String[] args) {
+        int[] idadeAluno = {20, 32, 45, 18};
+        float[][] notasAlunos = {{10.0f}, {7.0f, 8.6f}};
+    }
+}
